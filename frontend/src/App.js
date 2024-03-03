11079bdf3f211fd8b9ac4f08003be3a530501a8c
NEK1FEKS <nikita-pol98@mail.ru>
2024-03-03 17:26:34 +0300

    add route to main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './Components/LoginPage.js';
 import Page404 from './Components/Page404.js';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="login" element={<LoginPage />} />
-        <Route path="*" element={<Page404 />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="*" element={<Page404 />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
