1fe6b057223b9dd1897c705459fb6dce0127cdae
NEK1FEKS <nikita-pol98@mail.ru>
2024-03-05 18:20:39 +0300

    added channels section

---
+++ frontend/src/Components/ChannelsSection.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ChannelsSection = () => (
+  <p>Каналы</p>
+);
+
+export default ChannelsSection;
--- frontend/src/Components/MainPage.jsx
+++ frontend/src/Components/MainPage.jsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import routes from '../hooks/routes';
 import { fetchChannelsList } from '../slices/channelsSlice';
+import ChannelsSection from './ChannelsSection';
 
 const getAuthHeader = () => {
   const userId = JSON.parse(localStorage.getItem('userId'));
@@ -31,7 +32,13 @@ const MainPage = () => {
   console.log(channels, currentChannelId);
 
   return (
-    <div className="container h-100 my-4 overflow-hidden rounded shadow" />
+    <div className="container h-100 my-4 overflow-hidden rounded shadow">
+      <div className="row h-100 bg-white flex-md-row">
+        <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
+          <ChannelsSection />
+        </div>
+      </div>
+    </div>
   );
 };
 
