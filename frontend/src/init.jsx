e66f1a31d9a7d819855ce713193cc7a92ef3a979
NEK1FEKS <nikita-pol98@mail.ru>
2024-03-17 15:01:55 +0300

    fixed error of moving to the channel

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -11,7 +11,8 @@ const init = async (socket) => {
   const api = {
     sendMessage: (...args) => socket.emit('newMessage', ...args),
     createChannel: (...args) => new Promise((resolve, reject) => {
-      socket.timeout(3000).emit('newChannel', ...args, (error, response) => {
+      const [channelData] = args;
+      socket.timeout(3000).emit('newChannel', { ...channelData, createdBy: socket.id }, (error, response) => {
         const { data } = response;
         if (error) {
           console.error(error);
@@ -28,8 +29,11 @@ const init = async (socket) => {
     store.dispatch(actions.addMessage({ message: payload }));
   });
   socket.on('newChannel', (payload) => {
+    const { createdBy } = payload;
     store.dispatch(actions.addChannel({ channel: payload }));
-    store.dispatch(actions.setCurrentChannel(payload.id));
+    if (createdBy === socket.id) {
+      store.dispatch(actions.setCurrentChannel(payload.id));
+    }
   });
   socket.on('removeChannel', (payload) => {
     store.dispatch(actions.removeChannel({ channelId: payload.id }));
